8713a1a0786fec51d23bbeeff6cecc2350d9768c
Patrick S. Vogt <patrick.vogt@unibas.ch>
2011-08-31 12:53:04 +0200

    remove unused class

--- src/ch/unibas/urz/android/perssearch/view/ChooseAccount.java
+++
@@ -1,7 +0,0 @@
-package ch.unibas.urz.android.perssearch.view;
-
-public class ChooseAccount {
-	// AccountManager accountManager = AccountManager.get(context);
-	// Account[] accounts = accountManager.getAccounts();
-	// accountManager.
-}
