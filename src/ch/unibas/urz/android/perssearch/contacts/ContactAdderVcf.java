d8bde03553e9e19d85bef9273d05b85190e18584
Patrick S. Vogt <patrick.vogt@unibas.ch>
2011-09-01 15:37:16 +0200

    delete temp files

--- src/ch/unibas/urz/android/perssearch/contacts/ContactAdderVcf.java
+++ src/ch/unibas/urz/android/perssearch/contacts/ContactAdderVcf.java
@@ -1,6 +1,7 @@
 package ch.unibas.urz.android.perssearch.contacts;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -8,11 +9,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.Handler;
 import android.widget.Toast;
 import ch.unibas.urz.android.perssearch.helper.Logger;
 import ch.unibas.urz.android.perssearch.model.Person;
 
 public class ContactAdderVcf {
+
+	private static final int HOUR_IN_MILLIES = 1000 * 60 * 60;
+	private static final String FILENAME_PREFIX = "perssearch";
+	private static final String FILENAME_EXTENTION = ".vcf";
 
 	public static void addContact(Context ctx, Person person) {
 		try {
@@ -27,11 +33,41 @@ public class ContactAdderVcf {
 	}
 
 	private static File getVcfFile(Context ctx, Person person) throws IOException {
-		File tmpFile = File.createTempFile("perssearch", ".vcf", Environment.getExternalStorageDirectory());
+		// FIXME: move to subdir and cleanup
+		final File dir = new File(Environment.getExternalStorageDirectory(), "perssearch/tmp");
+		if (!dir.isDirectory()) {
+			dir.mkdirs();
+		}
+		File tmpFile = File.createTempFile(FILENAME_PREFIX, FILENAME_EXTENTION, dir);
 		FileWriter fw = new FileWriter(tmpFile);
 		fw.write(person.getVcard());
 		fw.flush();
 		fw.close();
+		Handler h = new Handler();
+		h.post(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					final long now = System.currentTimeMillis();
+					File[] oldFiles = dir.listFiles(new FileFilter() {
+						@Override
+						public boolean accept(File file) {
+							if (now - file.lastModified() > HOUR_IN_MILLIES) {
+								if (file.getName().endsWith(FILENAME_EXTENTION)) {
+									return true;
+								}
+							}
+							return false;
+						}
+					});
+					for (File file : oldFiles) {
+						file.delete();
+					}
+				} catch (Exception e) {
+					Logger.i("Cannot delete temp files");
+				}
+			}
+		});
 		return tmpFile;
 	}
 
